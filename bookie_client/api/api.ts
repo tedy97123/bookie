464027af90c17de2770f9a8b5ee47125f804bd6b
tedy97123 <tedyyohanes97@gmail.com>
2024-04-01 17:42:07 -0700

    changeing

--- bookie_client/api/api.ts
+++ bookie_client/api/api.ts
@@ -1,12 +1,27 @@
 // // api.ts
 //
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
-import {GetBookResponse} from './types';
-import axios from 'axios';
+import {
+   GetBookResponse,
+ } from './types';
 //
 const localURL = 'http://localhost:8005';
 // const herokuURL = 'https://acrulifytest-79506d9ff655.herokuapp.com';
 
-function getBooks(title) {
-  axios.post(localURL + '/books/goodReads' , );
-}
+export const api = createApi({
+  baseQuery: fetchBaseQuery({baseUrl: localURL}),
+  reducerPath: 'api',
+  tagTypes: ['GetBooks', 'GetBooksByCategory', 'GetBookSales', 'GetAccess'],
+  endpoints: build => ({
+    postBookTitle: build.mutation<Array<GetBookResponse>, any>({
+      query: title => ({
+        url: '/books/goodReads',
+        method: 'POST',
+        body: title,
+      }),
+      invalidatesTags: ['GetBooks'],
+    }),
+  }),
+});
+
+export const {usePostBookTitleMutation} = api;
