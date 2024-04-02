c44902683093d90e9742b73dcf054f76b39c3cc9
Tedy Yohanes <91521891+tedy97123@users.noreply.github.com>
2024-04-02 01:07:22 +0000

    changes to app to implement global store

--- bookie_client/App.tsx
+++ bookie_client/App.tsx
@@ -9,10 +9,23 @@ import React from 'react';
 import {SafeAreaView} from 'react-native';
 import {Home} from './screens';
 import {Provider} from 'react-redux';
+import { api } from './api/api';
+import { configureStore } from '@reduxjs/toolkit';
+import { setupListeners } from '@reduxjs/toolkit/query';
+
+
+export const store = configureStore({
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefault) => getDefault().concat(api.middleware),
+});
+setupListeners(store.dispatch);
+
 function App(): React.JSX.Element {
   return (
     <SafeAreaView>
-      <Provider  >
+      <Provider store={store} >
         <Home />
       </Provider>
     </SafeAreaView>
